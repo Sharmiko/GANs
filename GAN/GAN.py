aa5093fa7cce6f774e79d46cb4b7ff72eb559c04
Sharmiko <38474067+Sharmiko@users.noreply.github.com>
2019-11-29 12:01:38 +0400

    Add files via upload

---
+++ GAN/GAN.py
@@ -0,0 +1,127 @@
+import torch.nn as nn
+
+
+class Generator(nn.Module):
+    
+    def __init__(self):
+        
+        super(Generator, self).__init__()
+        
+        self.fc1 = nn.Linear(100, 3136)
+        
+        self.upsample = nn.Upsample(scale_factor=2)
+        self.relu = nn.ReLU()
+        
+        self.conv1 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=5,
+                               stride=1, padding=2)
+        self.conv2 = nn.Conv2d(in_channels=128, out_channels=64, kernel_size=5,
+                               stride=1, padding=2)
+        self.conv3 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=5,
+                               stride=1, padding=2)
+        self.conv4 = nn.Conv2d(in_channels=64, out_channels=1, kernel_size=5,
+                               stride=1, padding=2)
+        
+        self.batch_norm_fc1 = nn.BatchNorm1d(3136)
+        self.batch_norm_conv1 = nn.BatchNorm2d(128)
+        self.batch_norm_conv2 = nn.BatchNorm2d(64)
+        self.batch_norm_conv3 = nn.BatchNorm2d(64)
+        
+        self.out = nn.Tanh()
+        
+    def forward(self, t):
+        
+        t = t
+        
+        t = self.fc1(t)
+        t = self.batch_norm_fc1(t)
+        t = self.relu(t)
+        
+        t = t.view(-1, 64, 7, 7)
+
+        t = self.upsample(t)
+        
+        t = self.conv1(t)
+        t = self.batch_norm_conv1(t)
+        t = self.relu(t)
+        
+        t = self.upsample(t)
+        
+        t = self.conv2(t)
+        t = self.batch_norm_conv2(t)
+        t = self.relu(t)
+        
+        t = self.conv3(t)
+        t = self.batch_norm_conv3(t)
+        t = self.relu(t)
+        
+        t = self.conv4(t)
+        t = self.relu(t)
+        
+        t = self.out(t)
+        
+        return t        
+
+
+class Discriminator(nn.Module):
+    
+    def __init__(self):
+        
+        super(Discriminator, self).__init__()
+        
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=64, kernel_size=5,
+                               stride=2, padding=1)
+        self.conv2 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=5,
+                               stride=2, padding=1)
+        self.conv3 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=5,
+                               stride=1, padding=1)
+        self.conv4 = nn.Conv2d(in_channels=128, out_channels=128, kernel_size=1,
+                               stride=1, padding=0)
+        
+        self.relu = nn.ReLU()
+        self.droput = nn.Dropout(p=0.4)
+        
+        self.out = nn.Linear(4 * 4 * 128, 1)
+        
+    def forward(self, t):
+        
+        t = t
+        
+        t = self.conv1(t)
+        t = self.relu(t)
+        t = self.droput(t)
+        
+        t = self.conv2(t)
+        t = self.relu(t)
+        t = self.droput(t)
+        
+        t = self.conv3(t)
+        t = self.relu(t)
+        t = self.droput(t)
+        
+        t = self.conv4(t)
+        t = self.relu(t)
+        t = self.droput(t)
+        
+        t = t.view(-1, 4 * 4 * 128)
+        
+        t = self.out(t)
+        
+        return t
+        
+        
+class GAN(nn.Module):
+    
+    def __init__(self):
+        
+        super(GAN, self).__init__()
+        
+        self.generator = Generator()
+        self.discriminator = Discriminator()
+        
+    def forward(self, t):
+        
+        generated = self.generator(t)
+        
+        predicted = self.discriminator(t)
+        
+        return generated, predicted
---
+++ GAN/train.py
@@ -0,0 +1,138 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.utils.data as utils
+import torch.optim as optim
+from torch.autograd import Variable
+
+import matplotlib.pyplot as plt
+
+from tqdm import tqdm
+
+from GAN import Generator, Discriminator
+
+device = ("cuda:0" if torch.cuda.is_available() else "cpu")
+
+# Load training data from numpy array and convert it to tensor
+train_data = torch.from_numpy(np.load('../data/train_camel.npy'))
+
+train_data = train_data.float()
+train_data /= 255.0
+
+# Create torch Dataset object
+train_data = utils.TensorDataset(train_data)
+
+
+# Create DataLoader object
+trainloader = utils.DataLoader(train_data, batch_size=32)
+
+# Create model
+
+generator = Generator()
+discriminator = Discriminator()
+
+generator.to(device)
+discriminator.to(device)
+
+# Create optimizer and loss function
+
+optimizerG = optim.Adam(generator.parameters(), lr=0.0002, betas=(0.5, 0.999))
+optimizerD = optim.Adam(discriminator.parameters(), lr=0.002, betas=(0.5, 0.999))
+
+criterion = nn.BCEWithLogitsLoss()
+criterion.to(device)
+
+# real and fake labels for training
+real_label = 1
+fake_label = 0
+
+# create latent vector for generator
+noise = torch.randn(32, 100, 1, 1, device=device)
+
+# Training loop
+
+img_list=[]
+iters = 0
+
+EPOCHS = 5
+
+for epoch in tqdm(range(EPOCHS)):
+    
+    for i, data in enumerate(trainloader, 0):
+        
+        ########################
+        # Train Generator #
+        ########################
+        
+        # format batch
+        images = data[0].to(device)
+        images = images.view(-1, 1, 28, 28)
+        real_imgs = Variable(images.type(torch.Tensor)).to(device)
+        b_size = len(images)
+        real = torch.full((b_size,), real_label, device=device).to(device)
+        fake = torch.full((b_size,), fake_label, device=device).to(device)
+        # zero Generator grads
+        optimizerG.zero_grad()
+        
+        # sample noise for generator
+        z = Variable(torch.Tensor(np.random.normal(0, 1, (b_size, 100)))).to(device)
+        
+        # generate images
+        gen_images = generator(z)
+        
+        # loss of generator
+        g_loss = criterion(discriminator(gen_images).view(-1), real)
+        
+        g_loss.backward()
+        optimizerG.step()
+        
+        #######################
+        # Train Discriminator #
+        #######################
+        
+        optimizerD.zero_grad()
+        
+        # loss of discriminator
+        real_loss = criterion(discriminator(real_imgs).view(-1), real)
+        fake_loss = criterion(discriminator(gen_images.detach()).view(-1), fake)
+        d_loss = (real_loss + fake_loss) / 2
+        
+        d_loss.backward()
+        optimizerD.step()
+        
+        if i % 500 == 0:
+            img_list.append(gen_images)
+      
+    print("Epoch {} / {}".format(epoch + 1, EPOCHS))
+    
+print("G loss: {} \t D loss: {}".format(g_loss, d_loss))    
+
+    
+# Save model state
+torch.save(generator.state_dict(), "../model-states/Gen-[{}-Epochs]".format(EPOCHS))
+torch.save(discriminator.state_dict(), "../model-states/Dis-[{}-Epochs]".format(EPOCHS))
+
+# Load generator and discriminator
+generator = Generator()
+generator.load_state_dict(torch.load('../model-states/Gen-[5-Epochs]'))
+
+discriminator = Discriminator()
+discriminator.load_state_dict(torch.load('../model-states/Dis-[5-Epochs]'))
+        
+# Helper function to show generated images
+def imshow(inputs):
+    # create subplots
+    row = len(inputs) // 6
+    fig, axes = plt.subplots(row, 6, figsize=(14, 6))
+    j = 0
+    # plot original images
+    for k in range(row):
+        for i in range(6):
+            axes[k, i].imshow(inputs[j].view(28,28).numpy(), cmap="gray")
+            j += 1
+    plt.show()
+    
+
+# show generated images from last epoch
+imshow(img_list[34].detach().cpu())    
+        
