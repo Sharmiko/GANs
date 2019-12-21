c7b47cc07daa4f43cb083c4c30141b682c0ee8e0
Giorgi Sharmiashvili <sharmiko123@gmail.com>
2019-12-21 12:41:52 +0400

    Fixed Unet Generator (added skipped connections)

--- CycleGAN/UnetGenerator.py
+++ CycleGAN/UnetGenerator.py
@@ -1,3 +1,4 @@
+import torch
 import torch.nn as nn
 
 class DownSample(nn.Module):
@@ -6,7 +7,8 @@ class DownSample(nn.Module):
         
         super(DownSample, self).__init__()
         
-        self.conv1 = nn.Conv2d(in_channels=in_channels, out_channels=out_channels,
+        self.conv1 = nn.Conv2d(in_channels=in_channels,
+                               out_channels=out_channels,
                                kernel_size=4, stride=2, padding=1)
         self.instance_norm = nn.InstanceNorm2d(out_channels)
         self.relu = nn.ReLU()
@@ -30,21 +32,23 @@ class UpSample(nn.Module):
         
         self.upsample = nn.Upsample(scale_factor=2, mode='bilinear', 
                                     align_corners=True)
-        self.conv1 = nn.Conv2d(in_channels=in_channels, out_channels=out_channels, 
+        self.conv1 = nn.Conv2d(in_channels=in_channels, 
+                               out_channels=out_channels, 
                                kernel_size=5, stride=1, padding=2)
         self.instance_norm = nn.InstanceNorm2d(out_channels)
         self.relu = nn.ReLU()
         
         
-    def forward(self, t):
+    def forward(self, t, skip_layer):
         
         t = t
         
-        t = self.upsample(t)
-        
+        t = self.upsample(t)        
         t = self.conv1(t)
         t = self.instance_norm(t)
         t = self.relu(t)
+        
+        t = torch.cat((t, skip_layer), dim=0)
         
         return t
     
@@ -63,26 +67,28 @@ class UnetGenerator(nn.Module):
         self.upsample1 = UpSample(256, 128)
         self.upsample2 = UpSample(128, 64)
         self.upsample3 = UpSample(64, 32)
-        self.upsample4 = UpSample(32, 3)
+        self.upsample4 = nn.Upsample(scale_factor=2, mode='bilinear',
+                                     align_corners=True)
         
-        self.out = nn.Tanh()
+        self.out = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=5,
+                             padding=2)
         
         
     def forward(self, t):
         
         t = t
+
+        d1 = self.downsample1(t)
+        d2 = self.downsample2(d1)
+        d3 = self.downsample3(d2)
+        d4 = self.downsample4(d3)
+    
+        u1 = self.upsample1(d4, d3)
+        u2 = self.upsample2(u1, d2)
+        u3 = self.upsample3(u2, d1)
+        u4 = self.upsample4(u3)
         
-        t = self.downsample1(t)
-        t = self.downsample2(t)
-        t = self.downsample3(t)
-        t = self.downsample4(t)
-        
-        t = self.upsample1(t)
-        t = self.upsample2(t)
-        t = self.upsample3(t)
-        t = self.upsample4(t)
-        
-        t = self.out(t)
+        t = self.out(u4)
 
         return t
         
